7eb9a1e5ffcfec028d1ab0dd4184149665286baa
Lorin Hochstein <lhochstein@netflix.com>
2020-05-17 11:47:26 -0700

    checkpoint

--- except.h
+++ except.h
@@ -1,8 +1,10 @@
 #include <setjmp.h>
 
+#define try if(setjmp(env)==0)
+#define catch else
+
+#define throw(e) strncpy(error, e, N); longjmp(env, 1);
+
 jmp_buf env;
 const int N = 256;
 char error[N] = {0};
-
-#define try if(setjmp(env)==0)
-#define catch else
--- try-except-c.c
+++ try-except-c.c
@@ -5,6 +5,34 @@
 
 #include "except.h"
 
+void print_stat_info(struct stat buf);
+
+int main(int argc, char *argv[]) {
+  struct stat buf;
+
+  try {
+
+    if(argc!=2) {
+      char err[128];
+      snprintf(err, 128, "Invalid number of arguments: %d", argc);
+      throw(err);
+    }
+
+    char *path = argv[1];
+    if(stat(path, &buf)<0) {
+      throw(strerror(errno));
+    }
+
+    print_stat_info(buf);
+
+  } catch {
+    fprintf(stderr, "Error: %s\n", error);
+    return -1;
+  }
+
+  return 0;
+}
+
 
 void print_stat_info(struct stat buf) {
   printf("device: %d\n", buf.st_dev);
@@ -13,31 +41,4 @@ void print_stat_info(struct stat buf) {
   printf("# of hard-links: %d\n", buf.st_nlink);
   printf("uid: %d\n", buf.st_uid);
   printf("gid: %d\n", buf.st_gid);
-}
-
-int main(int argc, char *argv[]) {
-
-  try {
-
-    if(argc!=2) {
-      snprintf(error, N, "Invalid number of arguments: %d", argc);
-      longjmp(env, 1);
-    }
-
-    char *path = argv[1];
-    struct stat buf;
-    if(stat(path, &buf)<0) {
-      strerror_r(errno, error, N);
-      longjmp(env, 1);
-    }
-
-    print_stat_info(buf);
-
-
-  } catch {
-    fprintf(stderr, "Error: %s\n", error);
-    return -1;
-  }
-
-  return 0;
 }
